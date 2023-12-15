4627199049c5910738db4b1417b939e170d6b88a
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2023-12-15 16:23:28 +0400

    init app

---
+++ environments/dev/common/config/params-local.php
@@ -0,0 +1,8 @@
+<?php
+$domain = 'wow-blog.pro';
+return [
+    'baseUrl'             => 'https://' . $domain,
+    'backendUrl'          => 'https://backend.' . $domain,
+    'domain'        => $domain,
+    'cookieDomain'        =>  '.' . $domain,
+];
