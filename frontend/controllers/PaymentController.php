29253f463ea67f222965d870282fef4de281633d
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-05-30 00:47:54 +0400

    prostoj

--- frontend/controllers/PaymentController.php
+++ frontend/controllers/PaymentController.php
@@ -3,6 +3,7 @@
 namespace frontend\controllers;
 
 use common\controllers\WebController;
+use common\models\invoice\Deposit;
 use yii\web\NotFoundHttpException;
 use common\components\web\AuthorizedControllerTrait;
 use Yii;
@@ -32,5 +33,29 @@ class PaymentController extends WebController
     public function actionResult()
     {
         return $this->render('result');
+    }
+
+    /**
+     *
+     * @return \yii\web\Response | string
+     * @throws NotFoundHttpException
+     */
+    public function actionCallback()
+    {
+        header('Content-type: application/json');
+
+        /** @var Deposit[] $deposits */
+        $deposits = Deposit::find()
+                           ->andWhere(['status' => Deposit::STATUS_WAIT_CONFIRM])
+                           ->andWhere('payment_id is not null')
+                           ->all();
+
+        foreach ($deposits as $deposit) {
+            $deposit->check();
+        }
+
+        return json_encode([
+                               'code' => 200,
+                           ],JSON_PRETTY_PRINT);
     }
 }
