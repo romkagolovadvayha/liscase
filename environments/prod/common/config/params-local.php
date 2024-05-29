193c96ac3f5421e7fef83f829fbe47792426ace4
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-05-29 16:23:07 +0400

    prostoj

---
+++ environments/prod/common/config/params-local.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'baseUrl'             => 'https://prostoj.store',
+    'backendUrl'          => 'https://backend.prostoj.store',
+    'domain'        => 'prostoj.store',
+    'cookieDomain'        =>  '.prostoj.store',
+];
