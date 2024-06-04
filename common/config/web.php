4da4123c3e076fee53804a1e9721604891e4cff3
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-06-04 17:40:45 +0400

    prostoj

--- common/config/web.php
+++ common/config/web.php
@@ -54,7 +54,7 @@ $config = [
                     'sourceMessageTable' => '{{%language_source}}',
                     'messageTable'       => '{{%language_translate}}',
                     'enableCaching'      => true,
-                    'cachingDuration'    => 60*5,
+                    'cachingDuration'    => 86400,
                 ],
             ],
         ],
