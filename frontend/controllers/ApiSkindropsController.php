624959941cac9bafffbcf224b8833d0826506b58
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-06-13 17:02:38 +0400

    prostoj

--- frontend/controllers/ApiSkindropsController.php
+++ frontend/controllers/ApiSkindropsController.php
@@ -32,6 +32,13 @@ class ApiSkindropsController extends WebController
 
     public function actionGodraw() {
         header('Content-type: application/json');
+        if ($this->getBearerToken() !== Yii::$app->params['skinDrops']['apiKey']) {
+           return json_encode([
+                            'code' => 'fail',
+                            'errorRu' => 'Вы не авторизованы',
+                            'errorEn' => 'Not auth',
+                        ]);
+        }
         $this->layout = 'service';
         $minOnline = 20;
 
@@ -211,5 +218,41 @@ class ApiSkindropsController extends WebController
         }
 
         return false;
+    }
+
+    /**
+     * Get header Authorization
+     * */
+    private function getAuthorizationHeader(){
+        $headers = null;
+        if (isset($_SERVER['Authorization'])) {
+            $headers = trim($_SERVER["Authorization"]);
+        }
+        else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+            $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+        } elseif (function_exists('apache_request_headers')) {
+            $requestHeaders = apache_request_headers();
+            // Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+            $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+            //print_r($requestHeaders);
+            if (isset($requestHeaders['Authorization'])) {
+                $headers = trim($requestHeaders['Authorization']);
+            }
+        }
+        return $headers;
+    }
+
+    /**
+     * get access token from header
+     * */
+    private function getBearerToken() {
+        $headers = $this->getAuthorizationHeader();
+        // HEADER: Get the access token from the header
+        if (!empty($headers)) {
+            if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+                return $matches[1];
+            }
+        }
+        return null;
     }
 }
