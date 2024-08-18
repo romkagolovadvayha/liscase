9b41f6c6a0cf41cb676dc3aff9b2f1c322b2901d
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-08-18 15:39:25 +0400

    prostoj

--- common/components/queue/stats/SaveStatsJob.php
+++ common/components/queue/stats/SaveStatsJob.php
@@ -105,127 +105,143 @@ class SaveStatsJob extends BaseObject implements JobInterface
                     }
                 }
             }
-            foreach ($request['reports'] as $item) {
-                $model = new Reports();
-                $model->steam_id = $item['steam_id'];
-                $model->recepient_steam_id = $item['recepient_steam_id'];
-                $model->reason = $item['reason'];
-                $model->created_at = $item['created_at'];
-                $model->server_tag = $this->serverTag;
-                $model->wipe = $wipeDate;
-                $model->save();
+            try {
+                foreach ($request['reports'] as $item) {
+                    $model = new Reports();
+                    $model->steam_id = $item['steam_id'];
+                    $model->recepient_steam_id = $item['recepient_steam_id'];
+                    $model->reason = $item['reason'];
+                    $model->created_at = $item['created_at'];
+                    $model->server_tag = $this->serverTag;
+                    $model->wipe = $wipeDate;
+                    $model->save();
 
-                $user = User::findBySteamId($item['steam_id']);
-                $reportUser = User::findBySteamId($item['recepient_steam_id']);
+                    $user = User::findBySteamId($item['steam_id']);
+                    $reportUser = User::findBySteamId($item['recepient_steam_id']);
 
-                $count = Reports::find()
-                    ->andWhere(['recepient_steam_id' => $reportUser->steam_id])
-                    ->andWhere(['wipe' => $wipeDate])
-                    ->count();
+                    $count = Reports::find()
+                        ->andWhere(['recepient_steam_id' => $reportUser->steam_id])
+                        ->andWhere(['wipe' => $wipeDate])
+                        ->count();
 
-                $kills = 0;
-                $deaths = 0;
-                $kd = 0;
-                $playtime = 0;
+                    $kills = 0;
+                    $deaths = 0;
+                    $kd = 0;
+                    $playtime = 0;
 
-                try {
-                    $stats = Statistics::find()
-                              ->andWhere(['IN', '`key`', ['playtime', 'kills', 'deaths']])
-                              ->andWhere(['steam_id' => $reportUser->steam_id])
-                              ->andWhere(['server_tag' => $this->serverTag])
-                              ->andWhere(['wipe' => $wipeDate])
-                              ->asArray()
-                              ->indexBy('key')
-                              ->all();
-                    $kills = !empty($stats['playtime']) ? $stats['playtime'] : 0;
-                    $deaths = !empty($stats['deaths']) ? $stats['deaths'] : 0;
-                    if ($deaths > 1) {
-                        $kd = $kills > 0 ? round($kills / $deaths, 1) : 0;
-                    } else {
-                        $kd = $kills;
+                    try {
+                        $stats = Statistics::find()
+                                  ->andWhere(['IN', '`key`', ['playtime', 'kills', 'deaths']])
+                                  ->andWhere(['steam_id' => $reportUser->steam_id])
+                                  ->andWhere(['server_tag' => $this->serverTag])
+                                  ->andWhere(['wipe' => $wipeDate])
+                                  ->asArray()
+                                  ->indexBy('key')
+                                  ->all();
+                        $kills = !empty($stats['playtime']) ? $stats['playtime'] : 0;
+                        $deaths = !empty($stats['deaths']) ? $stats['deaths'] : 0;
+                        if ($deaths > 1) {
+                            $kd = $kills > 0 ? round($kills / $deaths, 1) : 0;
+                        } else {
+                            $kd = $kills;
+                        }
+                        $playtime = !empty($stats['playtime']) ? $stats['playtime'] : 0;
+                    } catch (\Exception $e) {
+                        Yii::$app->telegramReports->sendMessage("SaveStatsJob:" . $e->getLine() . ":" . $e->getMessage());
                     }
-                    $playtime = !empty($stats['playtime']) ? $stats['playtime'] : 0;
-                } catch (\Exception $e) {}
 
-                $playHour = round($playtime/60, 1);
+                    $playHour = round($playtime/60, 1);
 
-                $message = "⚔ <b>Новая жалоба на игрока</b>" . PHP_EOL
-                    . "Отправил: {$user->username} ({$user->steam_id})" . PHP_EOL . PHP_EOL
-                    . "Подозреваемый: <a href=\"https://steamcommunity.com/profiles/{$reportUser->steam_id}\">{$reportUser->username}</a>" . PHP_EOL
-                    . "SteamId: {$reportUser->steam_id}" . PHP_EOL
-                    . "Причина: {$item['reason']}" . PHP_EOL
-                    . "Кол-во репортов на игрока: <b>{$count}</b>" . PHP_EOL
-                    . "Играл за вайп: {$playHour} ч." . PHP_EOL
-                    . "Убийств: {$kills}" . PHP_EOL
-                    . "Смертей: {$deaths}" . PHP_EOL
-                    . "К/Д: {$kd}" . PHP_EOL
-                    . "Сервер: {$server->name}";
+                    $message = "⚔ <b>Новая жалоба на игрока</b>" . PHP_EOL
+                        . "Отправил: {$user->username} ({$user->steam_id})" . PHP_EOL . PHP_EOL
+                        . "Подозреваемый: <a href=\"https://steamcommunity.com/profiles/{$reportUser->steam_id}\">{$reportUser->username}</a>" . PHP_EOL
+                        . "SteamId: {$reportUser->steam_id}" . PHP_EOL
+                        . "Причина: {$item['reason']}" . PHP_EOL
+                        . "Кол-во репортов на игрока: <b>{$count}</b>" . PHP_EOL
+                        . "Играл за вайп: {$playHour} ч." . PHP_EOL
+                        . "Убийств: {$kills}" . PHP_EOL
+                        . "Смертей: {$deaths}" . PHP_EOL
+                        . "К/Д: {$kd}" . PHP_EOL
+                        . "Сервер: {$server->name}";
 
-                $bans = "";
-                $bansExist = false;
-                $lastCheck = "";
-                $lastCheckExist = false;
-                try {
-                    $rustCheck = Yii::$app->rustCheck->getInfo($reportUser->steam_id);
-                    if (!empty($rustCheck['bans'])) {
-                        foreach ($rustCheck['bans'] as $ban) {
+                    $bans = "";
+                    $bansExist = false;
+                    $lastCheck = "";
+                    $lastCheckExist = false;
+                    try {
+                        $rustCheck = Yii::$app->rustCheck->getInfo($reportUser->steam_id);
+                        if (!empty($rustCheck['bans'])) {
+                            foreach ($rustCheck['bans'] as $ban) {
+                                $bansExist = true;
+                                $bans .= $ban['serverName'] . ":" . $ban['reason'] . "; Дата: " . (new \DateTime($ban['banDate']))->format('Y-m-d H:i:s') . PHP_EOL;
+                            }
+                        }
+                        if (!empty($rustCheck['last_check'])) {
+                            foreach ($rustCheck['last_check'] as $_lastCheck) {
+                                $lastCheckExist = true;
+                                $lastCheck .= $_lastCheck['serverName'] . "; Дата: " . (new \DateTime($_lastCheck['time']))->format('Y-m-d H:i:s') . PHP_EOL;
+                            }
+                        }
+                    } catch (\Exception $e) {
+                        Yii::$app->telegramReports->sendMessage("SaveStatsJob:" . $e->getLine() . ":" . $e->getMessage());
+                    }
+                    try {
+                        $banList = Steam::getBansGGRust($reportUser->steam_id);
+                        foreach ($banList as $banItem) {
                             $bansExist = true;
-                            $bans .= $ban['serverName'] . ":" . $ban['reason'] . "; Дата: " . (new \DateTime($ban['banDate']))->format('Y-m-d H:i:s') . PHP_EOL;
+                            $bans .= $banItem['server'] . ":" . $banItem['reason'] . "; Дата: " . (new \DateTime($ban['date']))->format('Y-m-d H:i:s') . "; Срок: " . $ban['expireDate'] . PHP_EOL;
                         }
+                    } catch (\Exception $e) {
+                        Yii::$app->telegramReports->sendMessage("SaveStatsJob:" . $e->getLine() . ":" . $e->getMessage());
                     }
-                    if (!empty($rustCheck['last_check'])) {
-                        foreach ($rustCheck['last_check'] as $_lastCheck) {
-                            $lastCheckExist = true;
-                            $lastCheck .= $_lastCheck['serverName'] . "; Дата: " . (new \DateTime($_lastCheck['time']))->format('Y-m-d H:i:s') . PHP_EOL;
+                    try {
+                        $banList = Steam::getBansRustRoom($reportUser->steam_id);
+                        foreach ($banList as $banItem) {
+                            $bansExist = true;
+                            $bans .= $banItem['server'] . ":" . $banItem['reason'] . "; Дата: " . (new \DateTime($ban['date']))->format('Y-m-d H:i:s') . "; Срок: " . $ban['expireDate'] . PHP_EOL;
                         }
+                    } catch (\Exception $e) {
+                        Yii::$app->telegramReports->sendMessage("SaveStatsJob:" . $e->getLine() . ":" . $e->getMessage());
                     }
-                } catch (\Exception $e) {}
-                try {
-                    $banList = Steam::getBansGGRust($reportUser->steam_id);
-                    foreach ($banList as $banItem) {
-                        $bansExist = true;
-                        $bans .= $banItem['server'] . ":" . $banItem['reason'] . "; Дата: " . (new \DateTime($ban['date']))->format('Y-m-d H:i:s') . "; Срок: " . $ban['expireDate'] . PHP_EOL;
-                    }
-                } catch (\Exception $e) {}
-                try {
-                    $banList = Steam::getBansRustRoom($reportUser->steam_id);
-                    foreach ($banList as $banItem) {
-                        $bansExist = true;
-                        $bans .= $banItem['server'] . ":" . $banItem['reason'] . "; Дата: " . (new \DateTime($ban['date']))->format('Y-m-d H:i:s') . "; Срок: " . $ban['expireDate'] . PHP_EOL;
-                    }
-                } catch (\Exception $e) {}
-                try {
-                    $banList = Steam::getBansMagicRust($reportUser->steam_id);
-                    foreach ($banList as $banItem) {
-                        $bansExist = true;
-                        $bans .= $banItem['server'] . ":" . $banItem['reason'] . "; Дата: " . (new \DateTime($ban['date']))->format('Y-m-d H:i:s') . "; Срок: " . $ban['expireDate'] . PHP_EOL;
-                    }
-                } catch (\Exception $e) {}
-                $hoursExist = false;
-                $hours = 0;
-                try {
-                    $games = Steam::getGameInfo($reportUser->steam_id);
-                    foreach ($games as $game) {
-                        if ($game['appid'] == 252490) {
-                            $hoursExist = true;
-                            $hours = $game['playtime_forever'];
-                            break;
+                    try {
+                        $banList = Steam::getBansMagicRust($reportUser->steam_id);
+                        foreach ($banList as $banItem) {
+                            $bansExist = true;
+                            $bans .= $banItem['server'] . ":" . $banItem['reason'] . "; Дата: " . (new \DateTime($ban['date']))->format('Y-m-d H:i:s') . "; Срок: " . $ban['expireDate'] . PHP_EOL;
                         }
+                    } catch (\Exception $e) {
+                        Yii::$app->telegramReports->sendMessage("SaveStatsJob:" . $e->getLine() . ":" . $e->getMessage());
                     }
-                } catch (\Exception $e) {}
+                    $hoursExist = false;
+                    $hours = 0;
+                    try {
+                        $games = Steam::getGameInfo($reportUser->steam_id);
+                        foreach ($games as $game) {
+                            if ($game['appid'] == 252490) {
+                                $hoursExist = true;
+                                $hours = $game['playtime_forever'];
+                                break;
+                            }
+                        }
+                    } catch (\Exception $e) {
+                        Yii::$app->telegramReports->sendMessage("SaveStatsJob:" . $e->getLine() . ":" . $e->getMessage());
+                    }
 
-                if ($hoursExist) {
-                    $hours = round($hours/60, 1);
-                    $message .=  PHP_EOL . "Часов в Steam: " . $hours;
-                }
-                if ($bansExist) {
-                    $message .=  PHP_EOL  . PHP_EOL . "Найдены баны на других проектах:" . PHP_EOL . $bans;
-                }
-                if ($lastCheckExist) {
-                    $message .=  PHP_EOL  . PHP_EOL . "Последние проверки игрока:" . PHP_EOL . $lastCheck;
-                }
+                    if ($hoursExist) {
+                        $hours = round($hours/60, 1);
+                        $message .=  PHP_EOL . "Часов в Steam: " . $hours;
+                    }
+                    if ($bansExist) {
+                        $message .=  PHP_EOL  . PHP_EOL . "Найдены баны на других проектах:" . PHP_EOL . $bans;
+                    }
+                    if ($lastCheckExist) {
+                        $message .=  PHP_EOL  . PHP_EOL . "Последние проверки игрока:" . PHP_EOL . $lastCheck;
+                    }
 
-                Yii::$app->telegramReports->sendMessage($message);
+                    Yii::$app->telegramReports->sendMessage($message);
+                }
+            } catch (\Exception $e) {
+                Yii::$app->telegramReports->sendMessage("SaveStatsJob:" . $e->getLine() . ":" . $e->getMessage());
             }
             $server->players = $request['server']['online'] + 5;
             $server->joined = $request['server']['join'];
