b6621a3732e63a49193806233eb21123d10c5415
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-09-09 21:58:41 +0400

    prostoj

--- frontend/views/bot/activate.php
+++ frontend/views/bot/activate.php
@@ -7,35 +7,49 @@ use yii\bootstrap5\Html;
 $this->title = Yii::t('common', 'Код активации');
 
 ?>
+<div class="container-fluid mb-5">
+    <div class="main_wrap server_info_page">
+        <aside>
+            <?= $this->render('@frontend/views/widgets/_servers'); ?>
+            <?php echo $this->render('@frontend/views/layouts/_promocode_line'); ?>
+            <?= $this->render('@frontend/views/widgets/_live'); ?>
+        </aside>
+        <main id="main" role="main">
+            <div class="main_child">
+                <div class="page_content">
+                    <div class="page_content_header">
+                        <h1><?= Yii::t('common', 'Код активации персонального Telegram-бота'); ?></h1>
+                    </div>
+                    <div class="page_content_body">
 
-<div class="white-box">
-    <div class="row">
-        <div class="col-md-9 col-xs-12">
-            <p class="text-bold"><?= Yii::t('common', 'Код активации персонального Telegram-бота'); ?>:</p>
+                        <div class="referal_link btn-clipboard"
+                             data-bs-toggle="tooltip"
+                             data-bs-placement="bottom"
+                             data-bs-title="<?=Yii::t('common', 'Скопировать код')?>"
+                             data-clipboard-text="<?=$userConfirmModel->code?>"
+                             data-message="<?=Yii::t('common', 'Код скопирован в буфер обмена!')?>">
+                            <div class="referal_link_title">
+                                <span><?=Yii::t('common', "Код активации персонального Telegram-бота")?></span> <i class="fas fa-copy"></i>
+                            </div>
+                            <div class="referal_link_link">
+                                        <span>
+                                            <?=$userConfirmModel->code?>
+                                        </span>
+                            </div>
+                        </div>
 
-            <h4>
-                <?= $userConfirmModel->code; ?><br />
+                        <?php
+                        $botLink = Html::a('@ProstojServerBot', 'https://t.me/ProstojServerBot', ['target' => '_blank']);
+                        ?>
 
-                <?= Html::a(Yii::t('common', 'Скопировать код'), 'javascript:;', [
-                    'class' => 'btn btn-default waves-effect waves-light btn-clipboard',
-                    'title' => Yii::t('common', 'Скопировать код'),
-                    'data'  => [
-                        'clipboard-text' => $userConfirmModel->code,
-                        'message'        => Yii::t('common', 'Код скопирован в буфер обмена!'),
-                    ],
-                ]); ?>
-            </h4>
-            <div>&nbsp;</div>
-
-            <?php
-            $botLink = Html::a('@ProstojServerBot', 'https://t.me/ProstojServerBot', ['target' => '_blank']);
-            ?>
-
-            <p>
-                <?= Yii::t('common', 'Скопируйте и вставьте это код в {botLink}', [
-                    'botLink' => $botLink
-                ])?>
-            </p>
-        </div>
+                        <p class="mt-2">
+                            <?= Yii::t('common', 'Скопируйте и вставьте это код в {botLink}', [
+                                'botLink' => $botLink
+                            ])?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
 </div>
