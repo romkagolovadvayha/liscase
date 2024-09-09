56c1e7d5eb21750466102458648eb75fe712ee3a
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-09-09 21:27:24 +0400

    prostoj

--- frontend/controllers/BotController.php
+++ frontend/controllers/BotController.php
@@ -2,12 +2,15 @@
 
 namespace frontend\controllers;
 
+use common\components\web\AuthorizedControllerTrait;
 use Yii;
 use common\controllers\WebController;
 use common\models\user\UserConfirmCode;
 
 class BotController extends WebController
 {
+    use AuthorizedControllerTrait;
+
     public function actionActivate()
     {
         $userConfirmModel = UserConfirmCode::createTypeTelegramBot(Yii::$app->user->id);
