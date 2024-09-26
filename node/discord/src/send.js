6969832b7b8fe7dd0f17710aadfc059972d11914
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-09-26 13:20:57 +0400

    prostoj

--- node/discord/src/send.js
+++ node/discord/src/send.js
@@ -21,7 +21,7 @@ con.connect(function(err) {
                 function presence(i = 0) {
                     con.query(sql, function (err, result2) {
                         if (err) throw err;
-                        const name = "Текущий онлайн: " + (result2[0].players + result2[0].joined);
+                        const name = "Текущий онлайн: " + (result2[0].players + result2[0].joined) + "/" + result2[0].max;
                         client.user.setPresence({
                             activities: [
                                 {
