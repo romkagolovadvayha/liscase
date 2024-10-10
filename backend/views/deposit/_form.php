9ffb1ee7f052311dd5770a0a1edc9dc3b114c2d9
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-10-11 03:04:15 +0400

    prostoj

--- backend/views/deposit/_form.php
+++ backend/views/deposit/_form.php
@@ -18,6 +18,8 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'status')->dropDownList(\common\models\invoice\Deposit::getStatusList(), []) ?>
 
+    <?= $form->field($model, 'created_at')->textInput() ?>
+
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
