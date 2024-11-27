b40fa85ddd4561ac5657d1e42635a22606b26dde
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-11-27 14:38:38 +0400

    prostoj

---
+++ clear.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#rm -rf ./frontend/web/assets/*
+echo "Frontend Cache Clear"
+
+#rm -rf ./backend/web/assets/*
+echo "Backend Cache Clear"
+
+./yii translate/clear-translate-cache 2>&1
+echo "Translate Cache Clear"
