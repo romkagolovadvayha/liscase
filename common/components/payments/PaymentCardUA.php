a541a5396ac00a26f46ee5078aca4d5ac6c99921
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-11-30 00:29:38 +0400

    prostoj

--- common/components/payments/PaymentCardUA.php
+++ common/components/payments/PaymentCardUA.php
@@ -26,6 +26,7 @@ class PaymentCardUA
             return $model->status;
         }
         $result = Yii::$app->anyPayApi->check($model->payment_id);
+        Yii::$app->telegramChats->sendMessage(json_encode($result));
         if (empty($result['result']) || empty($result['result']['payments']) || empty($result['result']['payments'][$model->payment_id])) {
             return false;
         }
