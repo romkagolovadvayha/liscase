dea4f81200265c01908c88b0651bc36efe5fdc4d
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-12-13 19:48:31 +0400

    prostoj

--- console/migrations/m241213_151447_raid.php
+++ console/migrations/m241213_151447_raid.php
@@ -17,7 +17,7 @@ class m241213_151447_raid extends Migration
             'user_id'    => self::INT_FIELD_NOT_NULL,
             'location'   => self::VARCHAR_FIELD,
             'explosives' => 'VARCHAR(255) DEFAULT "[]"',
-            'owners'     => 'LONGTEXT DEFAULT "[]"',
+            'owners'     => 'JSON DEFAULT "[]"',
             'notify'     => self::TINYINT_1_FIELD,
             'created_at' => self::TIMESTAMP_FIELD,
             'server_id'     => 'INT(11) NOT NULL',
