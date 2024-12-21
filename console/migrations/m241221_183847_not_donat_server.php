9f181ffa33eb319f4c2aae3bcdf08852202a0ee9
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-12-21 22:40:28 +0400

    prostoj

---
+++ console/migrations/m241221_183847_not_donat_server.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m241221_183847_not_donat_server
+ */
+class m241221_183847_not_donat_server extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('servers','is_store', 'TINYINT(1) UNSIGNED NOT NULL DEFAULT 0');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m241221_183847_not_donat_server cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m241221_183847_not_donat_server cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
