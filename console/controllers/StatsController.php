d21b335792b0c7953f7e7d739d0ac9c2dc06cb4c
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2024-12-30 23:04:08 +0400

    prostoj

--- console/controllers/StatsController.php
+++ console/controllers/StatsController.php
@@ -3,6 +3,7 @@
 namespace console\controllers;
 
 use common\models\servers\Servers;
+use common\models\statistics\Reports;
 use common\models\statistics\Statistics;
 use common\models\statistics\Kills;
 use common\models\statistics\Teams;
@@ -98,4 +99,57 @@ class StatsController extends Controller
         }
     }
 
+    /**
+     * stats/winner
+     */
+    public function actionWinner() {
+        /** @var Servers[] $servers */
+        $servers = Servers::find()
+                          ->andWhere(['status' => Servers::STATUS_ACTIVE])
+                          ->orderBy(['sort' => SORT_ASC])
+                          ->all();
+
+        $users = [];
+        foreach ($servers as $server) {
+            /** @var Statistics[] $statisticsPlaytime */
+            $statisticsPlaytime = Statistics::find()
+                                          ->andWhere(['server_tag' => $server->tag])
+                                          ->andWhere(['wipe' => $server->currentWipe()])
+                                          ->andWhere(['key' => 'playtime'])
+                                          ->andWhere(['>=', 'value', 10*60])
+                                          ->indexBy('steam_id')
+                                          ->all();
+
+
+            foreach ($statisticsPlaytime as $item) {
+                if (empty($item->user->telegram_chat_id)) {
+                    continue;
+                }
+                if (strtotime($item->user->last_visit_server_at) < strtotime('2024-12-27 00:00:01')) {
+                    continue;
+                }
+                $countQuery = Reports::find()
+                                     ->andWhere(['recepient_steam_id' => $item->steam_id])
+                                     ->andWhere(['wipe' => $server->currentWipe()])
+                                     ->andWhere(['server_tag' => $server->tag]);
+
+                $count = $countQuery->count();
+                if ($count > 10) {
+                    continue;
+                }
+
+                $users[] = [
+                    'username' => $item->user->username,
+                    'steam_id' => $item->steam_id,
+                ];
+            }
+        }
+        shuffle($users);
+        $index = 1;
+        foreach ($users as $user) {
+            echo "{$index}. {$user['username']} ({$user['steam_id']})" . PHP_EOL;
+            $index++;
+        }
+
+    }
 }
