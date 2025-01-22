f884ee0895ab52c0e6d63ec7503dcb49b8a5f0e1
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-01-22 19:17:38 +0400

    prostoj

--- common/models/invoice/Deposit.php
+++ common/models/invoice/Deposit.php
@@ -37,6 +37,7 @@ class Deposit extends \common\components\base\ActiveRecord
     const TYPE_PAYMENT_CARD_UA        = 15;
     const TYPE_PAYMENT_CARD_KZT        = 16;
     const TYPE_PAYMENT_CARD_YM        = 17;
+    const TYPE_PAYMENT_CARD_TINKOFF        = 18;
 
     const STATUS_WAIT_CONFIRM = 1;
     const STATUS_CANCELED     = 2;
@@ -144,6 +145,7 @@ class Deposit extends \common\components\base\ActiveRecord
             self::TYPE_PAYMENT_CARD_UA     => Yii::t('common', 'Оплата картой UA'),
             self::TYPE_PAYMENT_CARD_KZT     => Yii::t('common', 'Оплата картой Казахстан'),
             self::TYPE_PAYMENT_CARD_YM     => Yii::t('common', 'Оплата ЮMoney'),
+            self::TYPE_PAYMENT_CARD_TINKOFF     => Yii::t('common', 'Оплата Тинькофф'),
         ];
     }
 
@@ -153,19 +155,20 @@ class Deposit extends \common\components\base\ActiveRecord
     public static function getIconTypeList()
     {
         $icons = [
-            self::TYPE_PAYMENT_CARD      => '/images/payments/cards.svg',
-            self::TYPE_PAYMENT_SBP       => '/images/payments/sbp.svg',
+//            self::TYPE_PAYMENT_CARD      => '/images/payments/cards.svg',
+//            self::TYPE_PAYMENT_SBP       => '/images/payments/sbp.svg',
 //            self::TYPE_PAYMENT_YOOONEY     => '/images/payments/iomoney.png',
 //            self::TYPE_PAYMENT_STEAM_PAY     => '/images/payments/scp_logo.png',
 //            self::TYPE_PAYMENT_VISA     => '/images/payments/tron.svg',
 //            self::TYPE_PAYMENT_MIR     => '/images/payments/tron.svg',
-            self::TYPE_PAYMENT_PERFECT_MONEY     => '/images/payments/Perfect_Money.png',
+//            self::TYPE_PAYMENT_PERFECT_MONEY     => '/images/payments/Perfect_Money.png',
 //            self::TYPE_PAYMENT_TRON      => '/images/payments/tron.svg',
-            self::TYPE_PAYMENT_TRC20     => '/images/payments/tether.svg',
+//            self::TYPE_PAYMENT_TRC20     => '/images/payments/tether.svg',
 //            self::TYPE_PAYMENT_ERC20     => '/images/payments/tether.svg',
-            self::TYPE_PAYMENT_CARD_UA     => '/images/payments/cards.svg',
-            self::TYPE_PAYMENT_CARD_KZT     => '/images/payments/cards.svg',
-            self::TYPE_PAYMENT_CARD_YM     => '/images/payments/iomoney.png',
+//            self::TYPE_PAYMENT_CARD_UA     => '/images/payments/cards.svg',
+//            self::TYPE_PAYMENT_CARD_KZT     => '/images/payments/cards.svg',
+//            self::TYPE_PAYMENT_CARD_YM     => '/images/payments/iomoney.png',
+            self::TYPE_PAYMENT_CARD_TINKOFF     => '/images/payments/cards.svg',
         ];
 
         if (Yii::$app->language !== 'ru-RU') {
@@ -173,6 +176,7 @@ class Deposit extends \common\components\base\ActiveRecord
             unset($icons[self::TYPE_PAYMENT_SBP]);
             unset($icons[self::TYPE_PAYMENT_CARD_UA]);
             unset($icons[self::TYPE_PAYMENT_CARD_KZT]);
+            unset($icons[self::TYPE_PAYMENT_CARD_TINKOFF]);
         }
 
         return $icons;
@@ -211,6 +215,7 @@ class Deposit extends \common\components\base\ActiveRecord
             self::TYPE_PAYMENT_CARD_UA => [1000, 100000],
             self::TYPE_PAYMENT_CARD_KZT => [1000, 100000],
             self::TYPE_PAYMENT_CARD_YM => [1000, 100000],
+            self::TYPE_PAYMENT_CARD_TINKOFF => [10, 100000],
         ];
     }
 
