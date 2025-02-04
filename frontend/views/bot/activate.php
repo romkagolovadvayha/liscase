9a0485ed95c16a71d1b7fa9863e76c5a5d8e968a
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-02-04 16:28:50 +0400

    prostoj

--- frontend/views/bot/activate.php
+++ frontend/views/bot/activate.php
@@ -7,50 +7,38 @@ use yii\bootstrap5\Html;
 $this->title = Yii::t('common', 'Код активации');
 
 ?>
-<div class="container-fluid mb-5">
-    <div class="main_wrap server_info_page">
-        <aside>
-            <?=$this->render('@frontend/views/widgets/_buttons'); ?>
-            <?= $this->render('@frontend/views/widgets/_servers'); ?>
-            <?php echo $this->render('@frontend/views/layouts/_promocode_line'); ?>
-            <?= $this->render('@frontend/views/widgets/_live'); ?>
-        </aside>
-        <main id="main" role="main">
-            <div class="main_child">
-                <div class="page_content">
-                    <div class="page_content_header">
-                        <h1><?= Yii::t('common', 'Код активации персонального Telegram-бота'); ?></h1>
-                    </div>
-                    <div class="page_content_body">
 
-                        <div class="referal_link btn-clipboard"
-                             data-bs-toggle="tooltip"
-                             data-bs-placement="bottom"
-                             data-bs-title="<?=Yii::t('common', 'Скопировать код')?>"
-                             data-clipboard-text="<?=$userConfirmModel->code?>"
-                             data-message="<?=Yii::t('common', 'Код скопирован в буфер обмена!')?>">
-                            <div class="referal_link_title">
-                                <span><?=Yii::t('common', "Код активации персонального Telegram-бота")?></span> <i class="fas fa-copy"></i>
-                            </div>
-                            <div class="referal_link_link">
-                                        <span>
-                                            <?=$userConfirmModel->code?>
-                                        </span>
-                            </div>
-                        </div>
-
-                        <?php
-                        $botLink = Html::a('@' . Yii::$app->settings->get('tgbot_login'), 'https://t.me/' . Yii::$app->settings->get('tgbot_login'), ['target' => '_blank']);
-                        ?>
-
-                        <p class="mt-2">
-                            <?= Yii::t('common', 'Скопируйте и вставьте это код в {botLink}', [
-                                'botLink' => $botLink
-                            ])?>
-                        </p>
-                    </div>
+<section class="tasks">
+    <h2 class="tasks__title">
+        <?=Yii::t('common', 'Активация телеграм бота')?>
+    </h2>
+    <section class="page-stats__block-without-hover">
+        <section>
+            <header class="flex items-center justify-space-between mb-24 transition-all">
+                <h4 class="flex items-center gap-x-12">
+                    <?=Yii::t('common', "Код активации персонального Telegram-бота")?>
+                </h4>
+            </header>
+            <div>
+                <div class="relative mb-12 btn-clipboard"
+                     style="max-width: 280px"
+                     data-bs-toggle="tooltip"
+                     data-bs-placement="right"
+                     data-bs-title="<?=Yii::t('common', 'Скопировать код')?>"
+                     data-clipboard-text="<?=$userConfirmModel->code?>"
+                     data-message="<?=Yii::t('common', 'Код скопирован в буфер обмена!')?>">
+                    <input class="search search_pay" value="<?=$userConfirmModel->code?>" readonly="">
+                    <span class="icons icons_16px fas fa-copy"></span>
                 </div>
+                <?php
+                    $botLink = Html::a('@' . Yii::$app->settings->get('tgbot_login'), 'https://t.me/' . Yii::$app->settings->get('tgbot_login'), ['target' => '_blank']);
+                ?>
+                <p class="mt-24 p1 text-left">
+                    <?= Yii::t('common', 'Скопируйте и вставьте это код в {botLink}', [
+                        'botLink' => $botLink
+                    ])?>
+                </p>
             </div>
-        </main>
-    </div>
-</div>
+        </section>
+    </section>
+</section>
