d7edb7061a4ea453061786429010c7bb6ea3b9ae
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-02-18 14:41:17 +0400

    prostoj

--- frontend/assets/MainAsset.php
+++ frontend/assets/MainAsset.php
@@ -16,7 +16,7 @@ class MainAsset extends AssetBundle
 
     public $css = [
 //        'css/design/root-colors.min.css',
-        'css/design/styles.min.css?v=1.22',
+        'css/design/styles.min.css?v=1.23',
     ];
 
     public $js = [
