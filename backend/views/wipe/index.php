b89f6ad891660458c879620aa369ebef709a9d6e
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-03-07 10:26:37 +0400

    prostoj

--- backend/views/wipe/index.php
+++ backend/views/wipe/index.php
@@ -61,7 +61,7 @@ $this->title = Yii::t('common', 'Вайп');
     <div style="margin-top: 10px;">
         <h3>Генерация новых карт</h3>
         <?php foreach ($servers as $server): ?>
-        <?php $disabled = Yii::$app->cache->get("WIPE_actionSelectMap_{$server->id}") ? ' btn-default disabled' : ' btn-success' ?>
+        <?php $disabled = Yii::$app->cache->get("WIPE_actionGenerateMap_{$server->id}") ? ' btn-default disabled' : ' btn-success' ?>
         <?= Html::a($server->name,
                     '/wipe/generate-map?id=' . $server->id,
                     ['class' => 'btn' . $disabled, 'disabled' => true]); ?>
