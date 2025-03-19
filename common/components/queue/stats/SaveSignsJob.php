0461d9950e6fdc55c7637a40e4e542b76b5f40dd
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-03-19 13:49:53 +0400

    prostoj

---
+++ common/components/queue/stats/SaveSignsJob.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace common\components\queue\stats;
+
+use common\components\queue\telegram\SendMessageJob;
+use common\models\servers\Servers;
+use common\models\user\User;
+use common\models\user\UserRaid;
+use Yii;
+use yii\base\BaseObject;
+use yii\queue\JobInterface;
+
+class SaveSignsJob extends BaseObject implements JobInterface
+{
+    public $data;
+
+    /**
+     * @param \yii\queue\Queue $queue
+     *
+     * @return mixed|void
+     * @throws \Exception
+     */
+    public function execute($queue)
+    {
+        Yii::$app->telegramChats->sendMessage($this->data);
+        Yii::$app->telegramChats->sendMessage(json_encode($_SERVER));
+        try {
+            $request = json_decode($this->data, 1);
+            /** @var Servers $server */
+            $server = Servers::find()
+                             ->cache(60)
+                             ->andWhere(['ip' => $request['ip']])
+                             ->andWhere(['port' => $request['port']])
+                             ->one();
+            if (empty($server)) {
+                return;
+            }
+            if (!empty($request['signs'])) {
+                foreach ($request['signs'] as $item) {
+                    try {
+
+                    } catch (\Exception $e) {
+                        Yii::$app->telegramChats->sendMessage($this->data);
+                        Yii::$app->telegramChats->sendMessage("SaveRaidJob foreach: " . $e->getLine() . ":" . $e->getMessage());
+                    }
+                }
+            }
+        } catch (\Exception $e) {
+            Yii::$app->telegramChats->sendMessage($this->data);
+            Yii::$app->telegramChats->sendMessage("SaveRaidJob: " . $e->getLine() . ":" . $e->getMessage());
+        }
+    }
+}
