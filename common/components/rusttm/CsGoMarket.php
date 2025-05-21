8d889ff3fad2a1ae35f8c3797c6ffd3cbb90a41e
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-05-21 23:02:52 +0400

    prostoj

--- common/components/rusttm/CsGoMarket.php
+++ common/components/rusttm/CsGoMarket.php
@@ -33,8 +33,7 @@ class CsGoMarket
     public function buy($name, $price, $partner, $token): array
     {
         $secretKey = Yii::$app->settings->get('rusttm_secretKey');
-        $name = urlencode($name);
-        $name = str_replace('+', '%20', $name);
+        $name = rawurlencode($name);
         $url = $this->baseUrl . "/buy-for?key={$secretKey}&hash_name=".$name."&price={$price}&partner={$partner}&token={$token}";
         $response = Yii::$app->curl->get($url);
         if (empty($response)) {
