d3864132ddb7f2b2c429b6a0d6e59b173bbfa68e
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-06-03 20:56:06 +0400

    prostoj

--- frontend/config/web.php
+++ frontend/config/web.php
@@ -134,7 +134,14 @@ $config = [
             'clients' => [
                 'steam' => [
                     'class' => \common\components\oauth\Steam::class,
+                    'authUrl' => 'https://steamcommunity.com/openid/login',
+                    'transport' => 'yii\httpclient\CurlTransport',
                     'key' => $params['steamApiKey'],
+                    'requestConfig' => [
+                        'timeout' => 30,
+                        'sslVerifyPeer' => true,
+                        'sslCafile' => '/etc/ssl/certs/ca-certificates.crt', // обязательно укажите свой путь
+                    ],
                 ],
             ],
         ],
