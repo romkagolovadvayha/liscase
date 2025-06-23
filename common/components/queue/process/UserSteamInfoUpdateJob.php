97caea25eafbbae12cb6c5b8d93b52f721bf5f43
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-06-23 15:20:21 +0400

    prostoj

--- common/components/queue/process/UserSteamInfoUpdateJob.php
+++ common/components/queue/process/UserSteamInfoUpdateJob.php
@@ -2,6 +2,7 @@
 
 namespace common\components\queue\process;
 
+use common\components\helpers\CacheArrayHelper;
 use common\components\oauth\Steam;
 use common\models\user\User;
 use WebSocket\Client;
@@ -24,70 +25,18 @@ class UserSteamInfoUpdateJob extends BaseObject implements JobInterface
     public function execute($queue)
     {
         try {
-            $cacheKey = "UserSteamInfoUpdateJob_execute";
-            $steamList = [];
-            if (!empty(Yii::$app->cache->get($cacheKey))) {
-                $steamList = Yii::$app->cache->get($cacheKey);
+            $cacheKey = "steamIds_for_update";
+            /** @var User $user */
+            $user = User::find()
+                        ->andWhere(['steam_id' => $this->steamId])
+                        ->one();
+            if (!empty($user) && strtotime($user->updated_at) < time() - 5 * 60) {
+                CacheArrayHelper::withLock($cacheKey, function() use ($cacheKey) {
+                    CacheArrayHelper::addToCacheArray($cacheKey, $this->steamId);
+                });
             }
-            if (!in_array($this->steamId, $steamList)) {
-                /** @var User $user */
-                $user = User::find()
-                            ->andWhere(['steam_id' => $this->steamId])
-                            ->one();
-                if (!empty($user) && strtotime($user->updated_at) < time() - 5 * 60) {
-                    $steamList[] = $this->steamId;
-                }
-            }
-            if (count($steamList) > 5) {
-                $infoUsers       = Steam::getInfoUsers($steamList);
-                if (empty($infoUsers)) {
-                    Yii::$app->telegramChats->sendMessage('Ждем таймаут стима 60 сек.');
-                    sleep(60);
-                    return;
-                }
-                Yii::$app->telegramChats->sendMessage('Успешное обновление ' . count($infoUsers) . " аккаунтов.");
-                foreach ($infoUsers as $infoUser) {
-                    /** @var User $user */
-                    $user = User::find()
-                                  ->andWhere(['steam_id' => $infoUser['steamid']])
-                                  ->one();
-                    if (empty($user)) {
-                        Yii::$app->telegramChats->sendMessage('Пользователь для обновления не найден: ' . $infoUser['steamid']);
-                        continue;
-                    }
-                    $user->updated_at = date('Y-m-d H:i:s');
-                    $user->username = HtmlPurifier::process($infoUser['personaname']);
-                    $user->save();
-                    $avatar = $this->_loadImage($infoUser['avatarfull'], $infoUser['steamid']);
-                    $user->userProfile->name = HtmlPurifier::process($infoUser['personaname']);
-                    $user->userProfile->avatar = $avatar;
-                    $user->userProfile->save();
-                }
-                $steamList = [];
-            }
-            Yii::$app->cache->set($cacheKey, $steamList, 7*24*60*60);
         } catch (\Exception $ex) {
             Yii::$app->telegramChats->sendMessage('UserSteamInfoUpdateJob: ' . $ex->getMessage());
         }
-    }
-
-    private function _loadImage($imageUrl, $id) {
-        $uploadDir = \Yii::getAlias('@frontend/web');
-        $fileUrl = "/uploads/avatar/steam/{$id}.png";
-        $filePath = $uploadDir . $fileUrl;
-        if (file_exists($filePath)) {
-            Yii::$app->telegramChats->sendMessage('Удален старый аватар и загружен новый: ' . $imageUrl);
-            unlink($filePath);
-        }
-        if (!file_exists(dirname(dirname($filePath)))) {
-            mkdir(dirname(dirname($filePath)));
-            chmod(dirname(dirname($filePath)), 0777);
-        }
-        if (!file_exists(dirname($filePath))) {
-            mkdir(dirname($filePath));
-            chmod(dirname($filePath), 0777);
-        }
-        file_put_contents($filePath, file_get_contents($imageUrl));
-        return $fileUrl;
     }
 }
--- console/controllers/UserController.php
+++ console/controllers/UserController.php
@@ -2,17 +2,15 @@
 
 namespace console\controllers;
 
+use common\components\helpers\CacheArrayHelper;
+use common\components\oauth\Steam;
 use common\components\queue\process\UserSteamInfoUpdateJob;
-use common\models\box\Box;
-use common\models\profit\Profit;
-use common\models\servers\Servers;
 use common\models\user\User;
-use common\models\user\UserBox;
-use common\models\user\UserDrop;
-use common\models\user\UserTree;
+use Yii;
 use yii\base\BaseObject;
 use yii\console\Controller;
 use yii\db\StaleObjectException;
+use yii\helpers\HtmlPurifier;
 
 class UserController extends Controller
 {
@@ -33,4 +31,64 @@ class UserController extends Controller
         }
     }
 
+    /**
+     * user/update
+     *
+     * @throws \Exception
+     */
+    public function actionUpdate()
+    {
+        $cacheKey = "steamIds_for_update";
+        $steamList = CacheArrayHelper::withLock($cacheKey, function() use ($cacheKey) {
+            return CacheArrayHelper::popFromCacheArray($cacheKey, 63);
+        });
+        $infoUsers       = Steam::getInfoUsers($steamList);
+        if (empty($infoUsers)) {
+            Yii::$app->telegramChats->sendMessage('Ждем таймаут стима 60 сек.');
+            // Возврат обратно в очередь
+            CacheArrayHelper::withLock($cacheKey, function() use ($cacheKey, $steamList) {
+                CacheArrayHelper::pushBackToCacheArray($cacheKey, $steamList);
+            });
+            sleep(60);
+            return;
+        }
+        Yii::$app->telegramChats->sendMessage('Успешное обновление ' . count($infoUsers) . " аккаунтов.");
+        foreach ($infoUsers as $infoUser) {
+            /** @var User $user */
+            $user = User::find()
+                        ->andWhere(['steam_id' => $infoUser['steamid']])
+                        ->one();
+            if (empty($user)) {
+                Yii::$app->telegramChats->sendMessage('Пользователь для обновления не найден: ' . $infoUser['steamid']);
+                continue;
+            }
+            $user->updated_at = date('Y-m-d H:i:s');
+            $user->username = HtmlPurifier::process($infoUser['personaname']);
+            $user->save();
+            $avatar = $this->_loadImage($infoUser['avatarfull'], $infoUser['steamid']);
+            $user->userProfile->name = HtmlPurifier::process($infoUser['personaname']);
+            $user->userProfile->avatar = $avatar;
+            $user->userProfile->save();
+        }
+    }
+
+    private function _loadImage($imageUrl, $id) {
+        $uploadDir = \Yii::getAlias('@frontend/web');
+        $fileUrl = "/uploads/avatar/steam/{$id}.png";
+        $filePath = $uploadDir . $fileUrl;
+        if (file_exists($filePath)) {
+            Yii::$app->telegramChats->sendMessage('Удален старый аватар и загружен новый: ' . $imageUrl);
+            unlink($filePath);
+        }
+        if (!file_exists(dirname(dirname($filePath)))) {
+            mkdir(dirname(dirname($filePath)));
+            chmod(dirname(dirname($filePath)), 0777);
+        }
+        if (!file_exists(dirname($filePath))) {
+            mkdir(dirname($filePath));
+            chmod(dirname($filePath), 0777);
+        }
+        file_put_contents($filePath, file_get_contents($imageUrl));
+        return $fileUrl;
+    }
 }
