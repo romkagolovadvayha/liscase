a5ca3a91018d3256dd5c9c287902c6c8fdd4a7cc
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-06-25 02:47:39 +0400

    prostoj

--- backend/config/web.php
+++ backend/config/web.php
@@ -21,6 +21,40 @@ $config = [
             'hostInfo' => str_replace(['http://', 'https://', '/'], '', $params['backendUrl']),
             'baseUrl'  => $params['backendUrl'],
         ],
+        'view' => [
+            'class' => '\rmrevin\yii\minify\View',
+            //'enableMinify' => !YII_DEBUG,
+            'concatCss' => true, // concatenate css
+            'minifyCss' => true, // minificate css
+            'concatJs' => true, // concatenate js
+            'minifyJs' => true, // minificate js
+            'minifyOutput' => true, // minificate result html page
+            'webPath' => '@web', // path alias to web base
+            'basePath' => '@webroot', // path alias to web base
+            'minifyPath' => '@webroot/minify', // path alias to save minify result
+            'jsPosition' => [ \yii\web\View::POS_END ], // positions of js files to be minified
+            'forceCharset' => 'UTF-8', // charset forcibly assign, otherwise will use all of the files found charset
+            'expandImports' => true, // whether to change @import on content
+            'compressOptions' => ['extra' => true], // options for compress
+            'excludeFiles' => [],
+            'excludeBundles' => [],
+            'renderers' => [
+                'twig' => [
+                    'class' => 'yii\twig\ViewRenderer',
+                    'cachePath' => '@runtime/Twig/cache',
+                    // Array of twig options:
+                    'options' => [
+                        'auto_reload' => true,
+                    ],
+                    'globals' => [
+                        'html' => ['class' => '\yii\helpers\Html'],
+                        'url' => ['class' => '\yii\helpers\Url'],
+                    ],
+                    'uses' => ['yii\bootstrap'],
+                ],
+                // ...
+            ],
+        ],
         'assetManager' => [
             'class' => 'yii\web\AssetManager',
             'forceCopy' => YII_DEBUG,
