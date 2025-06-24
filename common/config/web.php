51ab9a0555a58efc4d6b0b50e0ca7ef2f53730b8
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-06-25 02:46:01 +0400

    prostoj

--- common/config/web.php
+++ common/config/web.php
@@ -62,22 +62,7 @@ $config = [
             ],
         ],
         'view' => [
-            'class' => '\rmrevin\yii\minify\View',
-            //'enableMinify' => !YII_DEBUG,
-            'concatCss' => true, // concatenate css
-            'minifyCss' => true, // minificate css
-            'concatJs' => true, // concatenate js
-            'minifyJs' => true, // minificate js
-            'minifyOutput' => true, // minificate result html page
-            'webPath' => '@web', // path alias to web base
-            'basePath' => '@webroot', // path alias to web base
-            'minifyPath' => '@webroot/minify', // path alias to save minify result
-            'jsPosition' => [ \yii\web\View::POS_END ], // positions of js files to be minified
-            'forceCharset' => 'UTF-8', // charset forcibly assign, otherwise will use all of the files found charset
-            'expandImports' => true, // whether to change @import on content
-            'compressOptions' => ['extra' => true], // options for compress
-            'excludeFiles' => [],
-            'excludeBundles' => [],
+            'class' => 'yii\web\View',
             'renderers' => [
                 'twig' => [
                     'class' => 'yii\twig\ViewRenderer',
