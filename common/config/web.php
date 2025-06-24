3394c1c5497c4a21a15448fc51460baece55240d
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-06-25 03:40:47 +0400

    prostoj

--- common/config/web.php
+++ common/config/web.php
@@ -63,7 +63,7 @@ $config = [
         ],
         'view' => [
             'class' => '\rmrevin\yii\minify\View',
-            'enableMinify' => false,
+            'enableMinify' => !YII_DEBUG,
             'concatCss' => true, // concatenate css
             'minifyCss' => true, // minificate css
             'concatJs' => true, // concatenate js
