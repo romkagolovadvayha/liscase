732296bd80b48bf92d83890aba694c7a1ed7195b
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-06-26 19:47:25 +0400

    prostoj

--- common/config/web.php
+++ common/config/web.php
@@ -228,7 +228,7 @@ $config = [
         ],
         'openAiSupport' => [
             'class' => \common\components\openAi\OpenAiSupport::class,
-            'model' => 'gpt-4', // или gpt-3.5-turbo
+            'model' => 'gpt-3.5-turbo', // или gpt-3.5-turbo
         ],
         's3Api'   => [
             'class'  => \common\components\storage\S3Api::class,
