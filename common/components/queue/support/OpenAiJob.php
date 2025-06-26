6895edd6702d19c8b8bf1a1af280365ca33ecd2e
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-06-26 21:27:38 +0400

    prostoj

--- common/components/queue/support/OpenAiJob.php
+++ common/components/queue/support/OpenAiJob.php
@@ -34,7 +34,7 @@ class OpenAiJob extends BaseObject implements JobInterface
     {
         echo $this->message . PHP_EOL;
         try {
-            sleep(180);
+            sleep(Yii::$app->settings->get('openAi_sleep'));
             $chat = Support::findOne($this->chatId);
             if ($chat->status !== Support::STATUS_OPEN) {
                 return;
