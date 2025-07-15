ce6a1e822e4753a68b2d9bfed1f053496e72e31b
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-07-16 00:24:09 +0400

    prostoj

---
+++ backend/views/settings/fields/video.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use common\models\site\SiteSetting;
+
+/** @var SiteSetting $item */
+
+?>
+
+<div class="setting_items_item_block">
+    <span class="setting_items_item_block_name">
+        <span><?= Html::encode($item->name) ?> <a href="/settings/update?id=<?=$item->id?>"><i class="fas fa-pen"></i></a></span>
+        <span class="setting_items_item_block_code"><?=$item->category?>_<?=$item->code?></span>
+    </span>
+    <?php if (!empty($item->value)): ?>
+        <span class="setting_items_item_block_image">
+            <video class="case-video second" playsinline="" preload="auto" loop="" autoplay="" style="height: 30px;">
+                <source type="video/webm" src="<?=Yii::$app->params['baseUrl']?><?=$item->value?>">
+            </video>
+        </span>
+    <?php endif; ?>
+    <span>
+        <?=Html::fileInput('settings[' . $item->id . ']', null, ['class' => 'form-control', 'accept' => '.webm'])?>
+    </span>
+</div>
