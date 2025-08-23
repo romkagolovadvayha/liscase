9d65d42619402370282b627b9727cd5eadf86d46
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-08-23 15:26:52 +0400

    prostoj

--- common/components/telegram/foreignSystem/PersonalBotSystem.php
+++ common/components/telegram/foreignSystem/PersonalBotSystem.php
@@ -252,6 +252,10 @@ class PersonalBotSystem extends AbstractSystem
             return Yii::$app->cache->get($cacheKey);
         }
         $user = User::findByChatId($chatId);
+        if ($user->is_telegram_blocked) {
+            $user->is_telegram_blocked = false;
+            $user->save(false);
+        }
         if (empty($user)) {
             $return = '🔒 Авторизуйтесь, чтобы получить награду!' . PHP_EOL . "Для авторизации напишите /start";
             return $return;
