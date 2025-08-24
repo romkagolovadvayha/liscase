667b4d7953f67672760b1e3950b59dd06b2454f0
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-08-24 04:42:34 +0400

    prostoj

--- console/controllers/ChatController.php
+++ console/controllers/ChatController.php
@@ -16,7 +16,7 @@ class ChatController extends Controller
     public function actionCheck()
     {
         $date = new \DateTime();
-        $date->modify('-30 minute');
+        $date->modify('-120 minute');
         /** @var Chats[] $messages */
         $messages = Chats::find()
             ->andWhere(['>', 'created_at', $date->format('Y-m-d H:i:s')])
