304b1be0f53ce81ea5c1c1d7a3831802536cad21
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-08-26 10:18:30 +0400

    prostoj

--- common/components/queue/stats/SkinsApprovedJob.php
+++ common/components/queue/stats/SkinsApprovedJob.php
@@ -25,7 +25,7 @@ class SkinsApprovedJob extends BaseObject implements JobInterface
             $request = json_decode($this->data, 1);
             Yii::$app->telegramChats->sendMessage(2);
             Yii::$app->telegramChats->sendMessage($request['approved_ids']);
-            $approvedIds = explode(',', $request['approved_ids']);
+            $approvedIds = $request['approved_ids'];
 
             Yii::$app->telegramChats->sendMessage(count($approvedIds));
             /** @var ServerSkin[] $skins */
