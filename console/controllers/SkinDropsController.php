0477c230528668c53836d0eeccf18f8e750773d6
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-08-27 00:17:25 +0400

    prostoj

--- console/controllers/SkinDropsController.php
+++ console/controllers/SkinDropsController.php
@@ -136,6 +136,7 @@ class SkinDropsController extends Controller
         foreach ($skins as $skin) {
             $info = ServerSkin::getInfoSkin($skin->skin_id);
             if (empty($info['tags']) && empty($info['result'])) {
+                print_r(1);
                 print_r($info);
                 sleep(5);
                 continue;
