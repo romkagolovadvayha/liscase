ff727ac8442b0f1284543297755a8996b1b77e16
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-08-26 10:11:02 +0400

    prostoj

---
+++ common/components/queue/stats/SkinsApprovedJob.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace common\components\queue\stats;
+
+use common\components\oauth\Steam;
+use common\models\serverskin\ServerSkin;
+use Yii;
+use yii\base\BaseObject;
+use yii\queue\JobInterface;
+
+class SkinsApprovedJob extends BaseObject implements JobInterface
+{
+    public $data;
+
+    /**
+     * @param \yii\queue\Queue $queue
+     *
+     * @return mixed|void
+     * @throws \Exception
+     */
+    public function execute($queue)
+    {
+        try {
+            $request = json_decode($this->data, 1);
+            $approvedIds = explode(',', $request['approved_ids']);
+
+            /** @var ServerSkin[] $skins */
+            $skins = ServerSkin::find()
+                               ->andWhere(['status' => ServerSkin::STATUS_ACTIVE])
+                               ->all();
+
+            $count = 0;
+            foreach ($skins as $skin) {
+                if (!in_array($skin->skin_id, $approvedIds)) {
+                    continue;
+                }
+                $count++;
+
+                if ($count < 10) {
+                    $message = "🟢 <b>Скин автоматически удален</b>" . PHP_EOL . "Skin ID: {$skin->skin_id}" . PHP_EOL
+                        . "Причина: Принят в игру";
+
+                    Yii::$app->telegramSupport->sendMessage($message, [], $skin->getImagePubUrl());
+                }
+            }
+
+            Yii::$app->telegramSupport->sendMessage("Всего отклонено скинов: {$count}");
+
+        } catch (\Exception $e) {
+            Yii::error("SkinsApprovedJob: " . $e->getMessage(), 'error');
+        }
+    }
+}
