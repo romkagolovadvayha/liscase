3d3e13f13f8d9b4d10aeda8bb37ca1e976e6b526
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-09-01 16:02:58 +0400

    prostoj

--- frontend/views/market/form-modal.php
+++ frontend/views/market/form-modal.php
@@ -141,7 +141,7 @@ $user = Yii::$app->user->identity;
 <footer class="px-24 pb-24">
     <?php if (Yii::$app->user->isGuest): ?>
         <button class="button-danger w-full">
-            <a href="/auth/oauth?authclient=steam" class="button button-primary button-size__s h-36" title="<?=Yii::t('common', 'Авторизация через Steam')?>">
+            <a href="/auth/oauth?authclient=steam" class="button button-primary w-full" title="<?=Yii::t('common', 'Авторизация через Steam')?>">
                 <span class="button__text">
                     <?=Yii::t('common', 'Войти через Steam')?>
                     <span class="icons icons_24px icons_24px_steam bg-white"></span>
