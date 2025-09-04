a38bdd615f578590c7f55b0f558d2d5f5f6cfff2
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-09-04 14:37:58 +0400

    prostoj

--- backend/models/TelegramConstructor.php
+++ backend/models/TelegramConstructor.php
@@ -171,6 +171,7 @@ class TelegramConstructor extends \yii\db\ActiveRecord
                 ], 60);
             }
             if (!empty($buttons) || empty($photo)) {
+                Yii::$app->telegramChats->sendMessage($message);
                 Yii::$app->queueTelegram->push(new SendMessageJob([
                                                                       'telegram_chat_id' => $user->telegram_chat_id,
                                                                       'message' => $message,
