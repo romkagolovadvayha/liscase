cc294fcd1b41357787c5800f6b35afffd1e9c24f
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-09-09 10:02:52 +0400

    prostoj

--- common/components/queue/stats/SaveTeamsJob.php
+++ common/components/queue/stats/SaveTeamsJob.php
@@ -24,6 +24,7 @@ class SaveTeamsJob extends BaseObject implements JobInterface
      */
     public function execute($queue)
     {
+        Yii::$app->telegramChats->sendMessage($this->data);
         try {
             $request = json_decode($this->data, 1);
 
