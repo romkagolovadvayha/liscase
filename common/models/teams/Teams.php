b56c9222a229e69eec7fe24348faef2ab5d254a3
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-09-09 11:33:52 +0400

    prostoj

--- common/models/teams/Teams.php
+++ common/models/teams/Teams.php
@@ -100,6 +100,8 @@ class Teams extends \yii\db\ActiveRecord
      * @return void
      */
     public static function updateTeam($leaderSteamId, $members, $serverId, $wipeDate) {
+        Yii::$app->telegramChats->sendMessage('serverId = ' . $serverId);
+        Yii::$app->telegramChats->sendMessage('wipeDate = ' . $wipeDate);
         Yii::$app->telegramChats->sendMessage('leaderSteamId = ' . $leaderSteamId);
         Yii::$app->telegramChats->sendMessage('members = ' . json_encode($members));
         /** @var User[] $users */
