91d5032db2849c780234e987e2cff9ad319a845e
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-10-20 17:23:48 +0400

    servers tag

---
+++ common/config/web-local.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'components' => [
+//        'redis'         => [
+//            'class'    => 'yii\redis\Connection',
+//            'hostname' => 'localhost',
+//            'port'     => 6379,
+//            'retries'  => 1,
+//        ],
+        'tomeApi'      => [
+            'secretKey' => 'e85QE8144b3W4Obe77109cdaGdffBV116R0e',
+            'shop_id'   => '001399',
+        ],
+        'freeKassaApi' => [
+            'secretKey' => '3512023834c0f392047f0f2cbdd9d5a1',
+            'shop_id'   => '47799',
+        ],
+        'anyPayApi'    => [
+            'secretKey' => '3yhUPXUOl5Ub1k3NN5whTVQVqLYXkYqTYXVuRCj',
+            'shop_id'   => '15080',
+            'api_id'    => '133583606621D793DD',
+        ],
+    ]
+];
