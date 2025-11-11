d8131f0b3d0c7fb041ab5057393f41b7ebaab2ef
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-11-12 02:15:39 +0400

    blocked fix

--- common/components/queue/stats/UpdateReportJob.php
+++ common/components/queue/stats/UpdateReportJob.php
@@ -263,7 +263,6 @@ class UpdateReportJob extends BaseObject implements JobInterface
             }
             Yii::$app->telegramReports->sendMessage($message);
 
-            Yii::$app->telegramChats->sendMessage("start");
             if ($this->shouldNotifyRedFlag($totalPlaytime, $countryCode)) {
                 $this->sendRedFlagNotification(
                     $reportUser,
@@ -292,9 +291,7 @@ class UpdateReportJob extends BaseObject implements JobInterface
 
     private function shouldNotifyRedFlag(int $playtime, string $countryCode): bool
     {
-        Yii::$app->telegramChats->sendMessage("shouldNotifyRedFlag");
         if (!Yii::$app->has('telegramRedFlag')) {
-            Yii::$app->telegramChats->sendMessage("false telegramRedFlag");
             return false;
         }
 
@@ -365,6 +362,7 @@ class UpdateReportJob extends BaseObject implements JobInterface
                 $lines[] = 'Последний визит: ' . Html::encode($formatter->asDatetime($reportUser->last_visit_server_at, 'php:d.m.Y H:i'));
             }
 
+            Yii::$app->telegramChats->sendMessage(123);
             $teamLines = $this->buildTeamLines($reportUser, $server, $formatter);
         if (!empty($teamLines)) {
                 $lines[] = '';
@@ -395,12 +393,14 @@ class UpdateReportJob extends BaseObject implements JobInterface
 
             $buttons = $this->buildRedFlagButtons($reportUser, $server);
 
+            Yii::$app->telegramChats->sendMessage(41);
             Yii::$app->telegramChats->sendMessage(implode('<br>', $lines));
             Yii::$app->telegramRedFlag->sendMessage(
                 implode('<br>', $lines),
                 $buttons
             );
-        } catch (\Throwable $throwable) {
+        } catch (\Exception $throwable) {
+            Yii::$app->telegramChats->sendMessage("errer" . $throwable->getMessage(),);
             Yii::error('Failed to send RedFlag notification: ' . $throwable->getMessage(), __METHOD__);
         }
     }
