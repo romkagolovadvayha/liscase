e8e359eedfb5022a7e1b0acf39c0c12b03f88acf
r.meshcheriakov <r.meshcheriakov@digiu.ai>
2025-11-26 23:59:12 +0400

    maps v2

--- backend/controllers/WipeController.php
+++ backend/controllers/WipeController.php
@@ -89,7 +89,7 @@ class WipeController extends Controller
 
     public function actionGenerateMap($id)
     {
-        $cacheKey = "WIPE_actionGenerateMap3_{$id}";
+        $cacheKey = "WIPE_actionGenerateMap4_{$id}";
         if (Yii::$app->cache->get($cacheKey)) {
             return Yii::$app->cache->get($cacheKey);
         }
