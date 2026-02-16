af36974e8265e438993c1b8db8bc016ad340b03d
Roman Mescheryakov <rom_ik@bk.ru>
2026-02-16 20:10:51 +0400

    shop

--- common/models/support/SupportMessage.php
+++ common/models/support/SupportMessage.php
@@ -2,6 +2,8 @@
 
 namespace common\models\support;
 
+use common\components\helpers\Role;
+use common\models\rcon\RconTasks;
 use common\models\user\User;
 use Yii;
 
@@ -21,6 +23,14 @@ use Yii;
  */
 class SupportMessage extends \yii\db\ActiveRecord
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+        $this->on(self::EVENT_AFTER_INSERT, [$this, 'notifyPlayerIfModeratorReply']);
+    }
     /**
      * {@inheritdoc}
      */
@@ -96,5 +106,68 @@ class SupportMessage extends \yii\db\ActiveRecord
     public function getUser()
     {
         return $this->hasOne(User::class, ['id' => 'user_id']);
+    }
+
+    /**
+     * Отправляет RCON команду для уведомления игрока, если сообщение отправлено модератором/администратором
+     * (не создателем тикета)
+     */
+    public function notifyPlayerIfModeratorReply()
+    {
+        // Пропускаем системные сообщения (без user_id)
+        if (empty($this->user_id)) {
+            return;
+        }
+
+        // Загружаем автора сообщения и тикет
+        $messageAuthor = $this->user;
+        $ticket = $this->support;
+
+        if (!$messageAuthor || !$ticket) {
+            return;
+        }
+
+        // Проверяем, что автор сообщения является модератором/администратором/саппортом
+        if (!$messageAuthor->canRoles([Role::ROLE_ADMIN, Role::ROLE_MODERATOR, Role::ROLE_SUPPORT])) {
+            return;
+        }
+
+        // Проверяем, что автор сообщения не является создателем тикета
+        if ($messageAuthor->id === $ticket->user_id) {
+            return;
+        }
+
+        // Получаем создателя тикета
+        $ticketOwner = $ticket->user;
+        if (!$ticketOwner || empty($ticketOwner->steam_id)) {
+            return;
+        }
+
+        // Получаем сервер из тикета
+        $server = $ticket->server;
+        if (!$server) {
+            // Если сервер не указан в тикете, пытаемся получить из пользователя
+            $server = $ticketOwner->server;
+        }
+
+        if (!$server) {
+            Yii::warning("Cannot send RCON notification: server not found for ticket #{$ticket->getNumber()}");
+            return;
+        }
+
+        // Формируем сообщение для уведомления
+        $notificationMessage = "Ваш вопрос был рассмотрен, ответ готов";
+        
+        // Формируем RCON команду
+        $steamId = $ticketOwner->steam_id;
+        $rconCommand = "support.notify {$steamId} \"{$notificationMessage}\"";
+
+        try {
+            // Выполняем RCON команду на конкретном сервере
+            RconTasks::execute($rconCommand, [$server->tag]);
+            Yii::info("RCON notification sent: {$rconCommand} on server {$server->tag}");
+        } catch (\Exception $e) {
+            Yii::error("Failed to send RCON notification: " . $e->getMessage());
+        }
     }
 }
