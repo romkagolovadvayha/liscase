7df7f3b56311ce8b5c41723f1ae7ff59e1b25b95
Roman Mescheryakov <rom_ik@bk.ru>
2026-02-23 14:54:39 +0400

    shop

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,12 +13,12 @@ class AppAsset extends AssetBundle
     public $sourcePath = '@backend/assets/sources';
 
     public $css = [
-        'scss/main.min.css?v=2.2',
+        'scss/main.min.css?v=2.3',
     ];
 
     public $js = [
         'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js',
-        'js/backend.js?v=2.1',
+        'js/backend.js?v=2.2',
     ];
 
     public $depends = [
