991108ac0d226c76f6ef5e189fc0824670e0b441
Roman Mescheryakov <rom_ik@bk.ru>
2026-03-17 01:47:59 +0400

    shop

--- api/controllers/v1/BlogController.php
+++ api/controllers/v1/BlogController.php
@@ -114,16 +114,6 @@ class BlogController extends BaseApiController
      */
     public function actionIndex()
     {
-        try {
-            if (Yii::$app->has('telegramChats')) {
-                Yii::$app->telegramChats->sendMessage(
-                    '[Blog API] language: ' . Yii::$app->language
-                );
-            }
-        } catch (\Throwable $e) {
-            // не ломаем action при ошибке отправки в Telegram
-        }
-
         $request = Yii::$app->request;
         $page = (int)$request->get('page', 1);
         $limit = (int)$request->get('limit', 10);
