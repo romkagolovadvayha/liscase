b2b202d82941cbc6ea06c423f761876704b57df2
Roman Mescheryakov <rom_ik@bk.ru>
2026-05-13 12:14:14 +0400

    shop

--- plugins/RustApp.cs
+++ plugins/RustApp.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
@@ -28,7 +28,7 @@ using Star = ProtoBuf.PatternFirework.Star;
 
 namespace Oxide.Plugins
 {
-    [Info("RustApp", "RustApp.io", "2.5.4")]
+    [Info("RustApp", "RustApp.io", "2.6.0")]
     public class RustApp : RustPlugin
     {
         #region Variables
@@ -49,6 +49,8 @@ namespace Oxide.Plugins
         private static JsonSerializer _jsonSerializer = JsonSerializer.CreateDefault(null);
 
         private static (string name, string value)[] _ApiHeaders = Array.Empty<(string, string)>();
+
+        private static readonly object _false = false; // avoid boxing for hooks
 
         #endregion
 
@@ -75,6 +77,7 @@ namespace Oxide.Plugins
                 public string level_url = ConVar.Server.levelurl;
                 // Thanks for the information, DezLife
                 public string level_image_url = MapUploader.ImageUrl;
+                public int world_size = ConVar.Server.worldsize;
                 public string description = ConVar.Server.description + " " + ConVar.Server.motd;
                 public string branch = ConVar.Server.branch;
 
@@ -2119,6 +2122,14 @@ namespace Oxide.Plugins
                 Unsubscribe(nameof(OnClientCommand));
             }
 
+#if OXIDE
+            _chatCommandPrefixes = Interface.Oxide.Config.Commands.ChatPrefix.ToArray();
+#endif
+
+#if CARBON
+            _chatCommandPrefixes = API.Commands.Command.Prefixes.Select(p => p.Value).ToArray();
+#endif
+
             timer.Once(1f, () =>
             {
                 MetaInfo.Read();
@@ -2329,11 +2340,11 @@ namespace Oxide.Plugins
 
         #region Chat hooks
 
+        private static string[] _chatCommandPrefixes = new[] { "/" };
+
         private object OnClientCommand(Connection connection, string text)
         {
-            if (!text.StartsWith("chat.say", StringComparison.OrdinalIgnoreCase)
-                || text.StartsWith("chat.say \"/", StringComparison.OrdinalIgnoreCase)
-                || text.StartsWith("chat.say /", StringComparison.OrdinalIgnoreCase))
+            if (!IsChatSayCommand(text) || HasCommandPrefix(text))
             {
                 return null;
             }
@@ -2350,15 +2361,47 @@ namespace Oxide.Plugins
                     SendMessage(player, msg);
                 }
 
-                return false;
+                return _false;
             }
 
             return null;
+
+            static bool IsChatSayCommand(string text)
+            {
+                return text.StartsWith("chat.say", StringComparison.OrdinalIgnoreCase);
+            }
+
+            static bool HasCommandPrefix(string text)
+            {
+                const int ChatSayLength = 8;
+
+                if (text.Length < ChatSayLength + 2)
+                {
+                    return false;
+                }
+
+                var textSpan = text.AsSpan().Slice(ChatSayLength); // here we have at least 2 chars in textSpan
+                textSpan = textSpan.TrimStart().TrimStart('"'); // remove whitespaces and quotes
+                if (textSpan.IsEmpty)
+                {
+                    return false;
+                } 
+
+                foreach (var prefix in _chatCommandPrefixes)
+                {
+                    if (textSpan.StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
         }
 
         private void OnPlayerChat(BasePlayer player, string message, ConVar.Chat.ChatChannel channel)
         {
-            if (channel != ConVar.Chat.ChatChannel.Team && channel != ConVar.Chat.ChatChannel.Global && channel != ConVar.Chat.ChatChannel.Local)
+            if (channel is not ConVar.Chat.ChatChannel.Team and not ConVar.Chat.ChatChannel.Global and not ConVar.Chat.ChatChannel.Local)
             {
                 return;
             }
@@ -2366,9 +2409,7 @@ namespace Oxide.Plugins
             _RustAppEngine?.ChatWorker?.SaveChatMessage(new CourtApi.PluginChatMessageDto
             {
                 steam_id = player.UserIDString,
-
                 is_team = channel == ConVar.Chat.ChatChannel.Team,
-
                 text = message
             });
         }
